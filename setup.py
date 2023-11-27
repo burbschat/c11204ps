a6aec14402aba453a92b88ecdb2ed738710d6a03
burbschat <b.urbschat@tum.de>
2023-11-28 00:35:24 +0100

    adjust setuptools for new code

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup
 
+
 def readme():
-    with open('README.rst') as f:
+    with open("README.md") as f:
         return f.read()
 
-setup(name='pyCLAWSps',
-      version       ='0.1.10',
-      description   ='Python Code for Power Supply Hamamatsu c11204-01/02',
-      long_description=readme(),
-      keywords      ='python hamamatsu power supply c11204',
-      url           ='https://github.com/malindasds/pyCLAWSps',
-      author        ='Malinda de Silva',
-      author_email  ='desilva@mpp.mpg.de',
-      license       ='gpl-3.0',
-      packages      =['pyCLAWSps'],
-      install_requires=['pySerial','numpy'],
-      classifiers=[
+
+setup(
+    name="pyCLAWSps",
+    version="0.2.0",
+    description="Python package for controlling a Hamamatsu c11204-01/02 power supply",
+    long_description=readme(),
+    keywords="python hamamatsu power supply c11204",
+    url="https://github.com/burbschat/pyCLAWSps",
+    author="Bela Urbschat",
+    author_email="urbschat@mpp.mpg.de",
+    license="gpl-3.0",
+    packages=["pyCLAWSps"],
+    install_requires=["pySerial"],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        ],
-      zip_safe      =False)
+    ],
+    zip_safe=False,
+)
