44fb7561d06e65ae0ce5adf42dd18144bebb9ad8
burbschat <b.urbschat@tum.de>
2023-11-28 00:13:47 +0100

    add docstrings

--- pyCLAWSps/__init__.py
+++ pyCLAWSps/__init__.py
@@ -1,30 +1,25 @@
-"""
-Python Code for Power Supply Hamamatsu c11204-01/02
----------------------------------------------------
-Can be used to control Hamamatsu c11204 power supply via python 3
+"""Python interface for Hamamatsu c11204-01/02 power supply.
 
+This essentially wraps the serial UART interface provided by the manufacturer.
+The original use case for this is use with a power supply board for the CLAWS
+scintillation detectors. The code was only ever tested with the CLAWS hardware.
 
-Copyright (C) 2019  Malinda de Silva
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-
+You should have received a copy of the GNU General Public License along with
+this program. If not, see `https://www.gnu.org/licenses/`.
 """
 
 import serial
 from serial.serialutil import SerialException
 import serial.tools.list_ports
-import binascii
 
 
 class CLAWSps:
@@ -40,15 +35,15 @@ class CLAWSps:
 
         Args:
             serial_port: Descriptor of the serial port (as e.g. "/dev/ttyUSB0")
-            or integer indexing all found serial ports that contain 'CP210' or
-            'Q_MPPC_CTL' in their name.
+                or integer indexing all found serial ports that contain 'CP210' or
+                'Q_MPPC_CTL' in their name.
             max_voltage: Maximum allowed voltage to be set. Chose wisely depending on your hardware.
             min_voltage: Minimum allowed voltage to be set.
 
         Raises:
             SerialException: Raised when no serial ports could be discovered on
-            the system. This is the case when no serial device is connected
-            (any device whatsoever, not only the power supply).
+                the system. This is the case when no serial device is connected
+                (any device whatsoever, not only the power supply).
         """
         # Internally used fixed values
         self._STX = "02"  # start of text
@@ -117,14 +112,40 @@ class CLAWSps:
             print(f"Short read: {len(rx)} vs {length}")
         return rx
 
-    def _convert_command(self, command):
-        # Converts command to hex form needed for serial data transfer
+    def _convert_command(self, command: str) -> tuple[str, int]:
+        """Converts command to hex form needed for serial data transfer.
+
+        Args:
+            command: Command in string representation.
+
+        Returns:
+            Hex representation of command and checksum corresponding to command.
+        """
         command_bytes = command.encode()
         command_hex = command_bytes.hex()  # Get hex representation of command
         command_checksum = sum(bytearray(command_bytes))  # Compute checksum of command
         return command_hex, command_checksum
 
-    def _send_serial_command(self, command: str, value=0, response_length: int = 8):
+    def _send_serial_command_nocheck(self, command: str, value: int = 0, response_length: int = 8) -> bytes:
+        """Send serial command as specified in manual (i.e. using string representation).
+
+        For commands setting a value the `value` argument should be set
+        accordingly. If no value has to be passed it should be zero, its
+        default value.
+
+        The response is not checked for any errors that may be reported by the
+        c11204.
+
+        Args:
+            command: String representation of command (like `HRE`, `HGC`, etc.).
+            value: Value to pass with command
+                response_length: Length of bytes of the response. The given number
+                of bytes will be read from the serial port after sending the
+                command.
+
+        Returns:
+            Response read from serial port.
+        """
         self._ser.flushInput()
         self._ser.flushOutput()
         command_hex, command_checksum = self._convert_command(command)
@@ -144,119 +165,22 @@ class CLAWSps:
         rx = self._read(response_length)
         return rx
 
-    def _send_serial_command_checkresp(
-        self, command: str, value=0, command_response: (str | None) = None, response_length: int = 8
-    ) -> bytes:
-        # Additionally check the response and raise an error accordingly if
-        # some error is reported.
-        if command_response is None:
-            # The usual response is the same as the command, except all
-            # lowercase. Commands are usually all uppercase.
-            command_response = command.lower()
-        rx = self._send_serial_command(command, value, response_length)
-        if rx[1:4] == command_response.encode():
-            return rx
-        elif rx[1:4] == b"hxx":
-            self._checkerror(rx[4:8])  # Must raise an error!
-        else:
-            raise serial.SerialException("Unexpected respose.")
+    def _checkerror(self, rx: bytes):
+        """Check response against a set of errors defined in the c11204 manual.
 
-    def hv_disable(self) -> bytes:
-        "Set power supply High Voltage OFF"
-        return self._send_serial_command_checkresp("HOF")
+        Args:
+            rx: Response received from power supply after sending a command.
 
-    def hv_enable(self) -> bytes:
-        "Set power supply High Voltage ON"
-        return self._send_serial_command_checkresp("HON")
+        Raises:
+            SerialException: UART communication error: Parity error, overrun error, framing error.
+            SerialException: Timeout error: This indicates that the self._CR has not been received within 1000ms of receiving the self._STX. The received packet is discarded.
+            SerialException: Syntax error: The beginning of the received command is other than self.STX, which indicates the length of the command or 256 byte.
+            SerialException: Checksum error: Indicates that the checksum does not match.
+            SerialException: Command error: Indicates that command is undefined.
+            SerialException: Parameter error: Indicates that codes other than ASCII code(0~F) are in the parameter.
+            SerialException: Parameter size error: Indicates that the data length of the parameter is outside the specified length.
 
-    def reset(self) -> bytes:
-        "Reset the power supply"
-        return self._send_serial_command_checkresp("HRE")
-
-    def set_voltage(self, voltage_dec) -> bytes:
         """
-        Sets the high voltage output to the voltage specified.
-        Arguments
-        ---------
-        voltage_dec : float
-            applied voltage
-            NOTE -  The applied voltage can be set upto 90 V by c11204 power supply.
-                    Change the upper voltage limit (self.V_lim_upper) as required by the MPPC in use
-        """
-        if voltage_dec > self.max_voltage:
-            raise ValueError(
-                f"Voltage was set to {voltage_dec}, which is out of the defined voltage range ({self.min_voltage}, {self.max_voltage}). Voltage is too high."
-            )
-        elif voltage_dec < self.min_voltage:
-            raise ValueError(
-                f"Voltage was set to {voltage_dec}, which is out of the defined voltage range ({self.min_voltage}, {self.max_voltage}). Voltage is too low."
-            )
-        else:
-            voltage_conv = float(voltage_dec) / self._voltage_conversion
-            value = int(round(voltage_conv))
-            return self._send_serial_command_checkresp("HBV", value)
-
-    def get_voltage(self) -> float:
-        """
-        Returns power supply voltage
-        Returns
-        -------
-        float
-            Voltage in Volts
-        """
-        rx = self._send_serial_command_checkresp("HGV")
-        voltage = int(rx[4:8], 16) * self._voltage_conversion
-        return voltage
-
-    def get_current(self) -> float:
-        """
-        Returns power supply current
-        Returns
-        -------
-        float
-            Current in mA
-        """
-        rx = self._send_serial_command_checkresp("HGC")
-        current = int(rx[4:8], 16) * self._current_conversion
-        return current
-
-    def get_power(self) -> float:
-        # Gets voltage and current at the same time. This could technically be
-        # more precise than calling HGV and HGC in succession, but the effect
-        # is probably negligible here.
-        rx = self._send_serial_command_checkresp("HPO", response_length=28)
-        voltage = int(rx[12:16], 16) * self._voltage_conversion  # in V
-        current_mA = int(rx[16:20], 16) * self._current_conversion  # in mA
-        return voltage * current_mA  # in mW
-
-    def get_temperature(self) -> float:
-        # TODO: The forumla in the command reference I have is a little
-        # unclear. Also, for CLAWS currently we use no temperature sensor.
-        # Despite of this the status says it's connected, which is a little
-        # concerning. Investigate this and maybe implement this function.
-        # Also consider funcitons for setting temperature compensation modes
-        # for completness.
-
-        # rx = self._send_serial_command_checkresp("HGT")
-        # temperature = self._temperature_conversion(int(rx[4:8], 16))
-        # return temperature
-        raise NotImplementedError
-
-    def close(self):
-        "Close self.serial port"
-        self._ser.close()
-
-    def get_status_raw(self) -> int:
-        # Can format nicely with something like `"%04x" % status_raw`
-        rx = self._send_serial_command_checkresp("HGS")
-        return int(rx[4:8])  # Return the int encoding the status
-
-    def get_status(self) -> dict:
-        "Prints status information on the power supply (similar to getMonitorInfo()) but without voltage and current values"
-        status_hex = self.get_status_raw()
-        return self.parse_status(status_hex)
-
-    def _checkerror(self, rx):
         # Raise exceptions indicating errors as described in manual
         if rx == b"0001":
             raise SerialException("UART communication error: Parity error, overrun error, framing error")
@@ -281,11 +205,187 @@ class CLAWSps:
                 "Parameter size error: This indicates that the data length of the parameter is outside the specified length"
             )
 
-    def parse_status(self, status: int) -> dict:
+    def _send_serial_command(
+        self, command: str, value: int = 0, command_response: (str | None) = None, response_length: int = 8
+    ) -> bytes:
+        """Send serial command as specified in manual (i.e. using string representation).
+
+        For commands setting a value the `value` argument should be set
+        accordingly. If no value has to be passed it should be zero, its
+        default value.
+
+        The response is checked against a set of errors specified in the c11204
+        manual. A exception indicating the error is raised if a given error was
+        encountered.
+
+        Args:
+            command: String representation of command (like `HRE`, `HGC`, etc.).
+            value: Value to pass with command
+            command_response: String representation of command response. This
+                is usually the same as the command, except in lowercase. Thus if
+                `command_response` is its default value of None, the lowercase
+                version of `command` will be automatically used.
+            response_length: Length of bytes of the response. The given number
+                of bytes will be read from the serial port after sending the
+
+        Returns:
+            Response read from serial port.
+        """
+        # Additionally check the response and raise an error accordingly if
+        # some error is reported.
+        if command_response is None:
+            # The usual response is the same as the command, except all
+            # lowercase. Commands are usually all uppercase.
+            command_response = command.lower()
+        rx = self._send_serial_command_nocheck(command, value, response_length)
+        if rx[1:4] == command_response.encode():
+            return rx
+        elif rx[1:4] == b"hxx":
+            self._checkerror(rx[4:8])  # Must raise an error!
+        else:
+            raise serial.SerialException("Unexpected respose.")
+
+    def hv_disable(self) -> bytes:
+        """Disable power supply high voltage.
+
+        Returns:
+            Response read from serial port.
+        """
+        return self._send_serial_command("HOF")
+
+    def hv_enable(self) -> bytes:
+        """Enable power supply high voltage.
+
+        Returns:
+            Response read from serial port.
+        """
+        return self._send_serial_command("HON")
+
+    def reset(self) -> bytes:
+        """Reset the power supply.
+
+        Returns:
+            Response read from serial port.
+        """
+        return self._send_serial_command("HRE")
+
+    def set_voltage(self, voltage_dec: float) -> bytes:
+        """Sets the high voltage output to the voltage specified.
+
+        The specified voltage will be rounded to the nearest value supported by
+        the voltage set UART command.
+
+        Exceptions are raised when the set voltage is out of the specified
+        range for a given instance. This is supposed to be used as a mechanism
+        to prevent setting too high voltages that may damage the used hardware.
+
+        The c11204 power supply supports up to 90V. If that is appropriate for
+        the connected hardware must be judged by the operator.
+
+        Args:
+            voltage_dec: Voltage to set in V.
+
+        Raises:
+            ValueError: Raised if voltage is out of the the defined range for the used hardware.
+
+        Returns:
+            Response read from serial port.
+        """
+        if voltage_dec > self.max_voltage:
+            raise ValueError(
+                f"Voltage was set to {voltage_dec}, which is out of the defined voltage range ({self.min_voltage}, {self.max_voltage}). Voltage is too high."
+            )
+        elif voltage_dec < self.min_voltage:
+            raise ValueError(
+                f"Voltage was set to {voltage_dec}, which is out of the defined voltage range ({self.min_voltage}, {self.max_voltage}). Voltage is too low."
+            )
+        else:
+            voltage_conv = float(voltage_dec) / self._voltage_conversion
+            value = int(round(voltage_conv))
+            return self._send_serial_command("HBV", value)
+
+    def get_voltage(self) -> float:
+        """Get voltage from power supply.
+
+        Returns:
+            Voltage in V.
+        """
+        rx = self._send_serial_command("HGV")
+        voltage = int(rx[4:8], 16) * self._voltage_conversion
+        return voltage
+
+    def get_current(self) -> float:
+        """Get current from power supply.
+
+        Returns:
+            Current in mA.
+        """
+        rx = self._send_serial_command("HGC")
+        current = int(rx[4:8], 16) * self._current_conversion
+        return current
+
+    def get_power(self) -> float:
+        """Get power from power supply.
+
+        This uses a provided UART command which gets voltage and current at the
+        same time. This could technically be more precise than calling HGV and
+        HGC in succession, but the effect is probably negligible here.
+
+        Returns:
+            Power in mW.
+        """
+        rx = self._send_serial_command("HPO", response_length=28)
+        voltage = int(rx[12:16], 16) * self._voltage_conversion  # in V
+        current_mA = int(rx[16:20], 16) * self._current_conversion  # in mA
+        return voltage * current_mA  # in mW
+
+    def get_temperature(self) -> float:
+        """Not yet implemented."""
+        # TODO: The forumla in the command reference I have is a little
+        # unclear. Also, for CLAWS currently we use no temperature sensor.
+        # Despite of this the status says it's connected, which is a little
+        # concerning. Investigate this and maybe implement this function.
+        # Also consider funcitons for setting temperature compensation modes
+        # for completness.
+
+        # rx = self._send_serial_command_checkresp("HGT")
+        # temperature = self._temperature_conversion(int(rx[4:8], 16))
+        # return temperature
+        raise NotImplementedError
+
+    def close(self):
+        """Close serial port."""
+        self._ser.close()
+
+    def get_status_raw(self) -> int:
+        """Get status of the power supply. 
+
+        The status is encoded in an integer value which has to be parsed to
+        extract the different status flags.
+
+        Returns:
+            Integer encoding the current status of the power supply.
+        """
+        # Can format nicely with something like `"%04x" % status_raw`
+        rx = self._send_serial_command("HGS")
+        return int(rx[4:8])  # Return the int encoding the status
+
+    def _parse_status(self, status: int) -> dict:
+        """Convert integer representation of power supply status to dictionary representation.
+
+        Args:
+            status: Integer representation of power supply status.
+
+        Returns:
+            Dictionary where keys are status flags that are either True or
+            False (dict values).
+        """
         # Use shorthand to make the following code less convoluted
         s = status
 
-        # TODO: Look up status codes and check if the namings are appropriate
+        # TODO: Look up status codes and check if the namings are really
+        # appropriate. Some were not contained in the manual I have, but it is
+        # also a rather old manual.
         status_dict = {
             "high_voltage_output": bool(s & 1),
             "overcurrent_protection": bool(s & 2),
@@ -299,3 +399,13 @@ class CLAWSps:
             "voltage_stable": bool(s & 0x4000),
         }
         return status_dict
+
+    def get_status(self) -> dict:
+        """Get status of the power supply represented by values of a dictionary.
+
+        Returns:
+            Dictionary where keys are status flags that are either True or
+            False (dict values).
+        """
+        status_hex = self.get_status_raw()
+        return self._parse_status(status_hex)
